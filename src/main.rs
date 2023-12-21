449e17d55cef0841293194493f15702a33e40974
Nguyen Huu Kim <13513658+kimyvgy@users.noreply.github.com>
2023-12-21 15:45:10 +0700

    refactor: Nicer error reporting with anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -9,9 +10,10 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path)?;
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
